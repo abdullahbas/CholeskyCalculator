95b16ed58cfbd62bbf90daadde4fb60be6a08318
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2020-06-28 19:13:41 +0300

    Add files via upload

---
+++ Cholesky/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Akifff
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.calis = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sonuc = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 89);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(628, 394);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.Click += new System.EventHandler(this.richTextBox1_Click);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // calis
+            // 
+            this.calis.Location = new System.Drawing.Point(12, 22);
+            this.calis.Name = "calis";
+            this.calis.Size = new System.Drawing.Size(264, 51);
+            this.calis.TabIndex = 15;
+            this.calis.Text = "Denklemi Al";
+            this.calis.UseVisualStyleBackColor = true;
+            this.calis.Click += new System.EventHandler(this.calis_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(646, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(467, 404);
+            this.listBox1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Değişken ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(623, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "x";
+            // 
+            // sonuc
+            // 
+            this.sonuc.Location = new System.Drawing.Point(282, 22);
+            this.sonuc.Name = "sonuc";
+            this.sonuc.Size = new System.Drawing.Size(239, 51);
+            this.sonuc.TabIndex = 20;
+            this.sonuc.Text = "Çalıştır";
+            this.sonuc.UseVisualStyleBackColor = true;
+            this.sonuc.Click += new System.EventHandler(this.sonuc_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(760, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 51);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 495);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sonuc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.calis);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "CHOLESKY YÖNTEMİ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button calis;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sonuc;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Cholesky/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Akifff
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        public int denksay;
+        public float[,] matrix = new float[100, 100];
+        public float[] maty = new float[100];
+
+        private void calis_Click(object sender, EventArgs e)
+        {
+            // Split on one or more non-digit characters.
+            listBox1.Items.Clear();
+            richTextBox1.Text = richTextBox1.Text.Replace(".", ",");
+            denksay = richTextBox1.Lines.Count();
+
+            for (int kk = 0; kk <= richTextBox1.Lines.Count() - 1; kk++)
+            {
+                int i;
+                string[] input = richTextBox1.Lines[kk].ToString().Split('=');
+
+                string degis = textBox1.Text;
+                string[] parca = input[0].Split(degis[0]);
+
+
+                string buff;
+                int y;
+
+
+                for (i = 0; i < parca.Length; i++)
+                {
+
+
+                    //MessageBox.Show( kk.ToString() + " denklemin" + i.ToString() + ".parçası="+parca[i].ToString());
+
+
+
+                    if (i == 0)
+                    { if (parca[0] == "" || parca[0] == "-")
+                        {
+                            parca[0] = parca[0] + "1";
+                        }
+                        buff = parca[1];
+                        matrix[kk, int.Parse(buff[0].ToString()) - 1] = float.Parse(parca[0].ToString());
+
+                    }
+
+                    else
+                    {
+                        if (parca[i].Length < 3)
+                        {
+                            parca[i] = parca[i] + "1";
+                        }
+                        buff = parca[i].ToString();
+
+                        try
+                        {
+                            string buff2 = parca[i + 1].ToString();
+                            matrix[kk, int.Parse(buff2[0].ToString()) - 1] = float.Parse(buff.Substring(1, buff.Length - 1).ToString());
+
+                        }
+                        catch
+                        {
+                            //MessageBox.Show("girdi");
+                        }
+
+
+
+                    }
+
+
+                }
+                string s = "";
+                for (int j = 0; j < richTextBox1.Lines.Count(); j++)
+                {
+                    if (matrix[kk, j] >= 0)
+                    {
+                        s = s + "    " + matrix[kk, j].ToString();
+                    }
+                    else
+
+                    {
+                        s = s + "  " + matrix[kk, j].ToString();
+                    }
+
+
+                }
+
+                s = "|" + s + "   |     " + input[1];
+                maty[kk] = float.Parse(input[1]);
+                listBox1.Items.Add(s);
+
+
+            }
+            listBox1.Items.Add("GİRİLEN DENKLEMLER.");
+            for (int ii = 0; ii <= richTextBox1.Lines.Count() - 1; ii++)
+            {
+                listBox1.Items.Add(richTextBox1.Lines[ii].ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.ForeColor = Color.Gray;
+        }
+
+        private void richTextBox1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.ForeColor == Color.Gray)
+            {
+                richTextBox1.ReadOnly = false;
+                richTextBox1.Text = "";
+                richTextBox1.BackColor = Color.White;
+                richTextBox1.ForeColor = Color.Black;
+            }
+        }
+        public float[,] u = new float[100, 100];
+        public float[,] L = new float[100, 100];
+        public float[] Y = new float [100];
+        public float[] x = new float[100];
+
+        private void sonuc_Click(object sender, EventArgs e)
+        {
+            /*  string[] parca = textBox1.Text.Split(',');
+              richTextBox1.AppendText("\r\n" + matrix[int.Parse(parca[0]),int.Parse(parca[1])]);
+              richTextBox1.ScrollToCaret();*/
+            richTextBox1.Text = "";
+       
+            for (int j=0;j<denksay;j++)
+            {
+
+                if (j==0)
+                {
+                    Y[j] = maty[j];  
+                }
+                else
+                {
+                    float toplam = 0;
+                    for (int i=0; i<=j-1;i++)
+                    {
+                        toplam = toplam + (float)(Y[i] * L[j, i]);
+                    }
+                    Y[j] = maty[j] - toplam;
+                }
+
+                for (int k=0;k<denksay ;k++)
+                {
+                    if (k==0)
+                    {
+                        u[k, j] = matrix[k, j];
+                    }
+
+                    if (j>=k)
+                    {
+                        if (k==j)
+                        {
+                            L[k, k] = 1;
+                        }
+                        float toplam=0;
+                        for (int i=0;i<=k-1;i++)
+                        {
+                            toplam = toplam +(float)(L[k, i] * u[i, j]);
+                        }
+                        u[k, j] = matrix[k, j] - toplam;
+
+
+                    }
+
+                    else
+                    {
+                        float toplam = 0;
+                        for (int i = 0; i <= j - 1; i++)
+                        {
+                            toplam = toplam + (float)L[k, i] * u[i, j];
+                        }
+                        L[k, j] = (matrix[k, j]-toplam)/u[j,j];
+
+                    }
+
+                }
+
+            }
+            for (int i = denksay-1 ; i >= 0; i--)
+            { 
+                float toplam = 0;
+                for (int j=i+1;j<=denksay;j++)
+                {
+                    toplam = toplam +(float) (u[i, j] * x[j]);
+
+                }
+                x[i] = (Y[i] - toplam) / u[i, i];
+               // MessageBox.Show( Y[i].ToString()+"  "+x[i].ToString()+"  "+u[i,i].ToString());
+            }
+
+            richTextBox1.AppendText(" U=  ");
+
+            for (int kk=0; kk<denksay; kk++)
+            {
+                richTextBox1.AppendText("\r");
+
+                for (int kk2=0;kk2<denksay;kk2++)
+                {
+                    richTextBox1.AppendText("   "+ u[kk,kk2].ToString());
+
+                }
+                
+
+            }
+
+            richTextBox1.AppendText("\r\r L=  ");
+            for (int kk = 0; kk < denksay; kk++)
+            {
+                richTextBox1.AppendText("\r");
+
+                for (int kk2 = 0; kk2 < denksay; kk2++)
+                {
+                    richTextBox1.AppendText("   " + L[kk, kk2].ToString());
+
+                }
+              
+
+            }
+
+            richTextBox1.AppendText("\r\r LY=b  \r\r Y=  \r ");
+
+            for (int kk=0; kk<denksay; kk++)
+            {
+                richTextBox1.AppendText("   " + Y[kk].ToString());
+
+            }
+            richTextBox1.AppendText("\r\r U"+textBox1.Text+ "=Y \r\r X=  \r ");
+
+            for (int kk = 0; kk < denksay; kk++)
+            {
+                richTextBox1.AppendText("   " + x[kk].ToString());
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*richTextBox1.AppendText("\r\n" + x[int.Parse(textBox1.Text)].ToString());
+            richTextBox1.ScrollToCaret();*/
+            richTextBox1.Text = "";
+            listBox1.Items.Clear();
+            Form ab = new Form1();
+            Form1.ActiveForm.Hide();
+            ab.Show();
+            
+
+
+
+
+
+
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
